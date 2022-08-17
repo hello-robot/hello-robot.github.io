e061f1d283b87b4b7658ad9af555e9039ef7fd1c
Binit Shah <bshah@hello-robot.com>
2022-08-17 12:04:45 -0700

    Fix install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ pip3 install mkdocs-material
 pip3 install mkdocs-simple-plugin
 #pip3 install mkdocstrings==0.17.0
 pip3 install pytkdocs[numpy-style]
-pip3 install jinja2=3.0.3
-pip3 install install Pygments
+pip3 install jinja2==3.0.3
+pip3 install Pygments
 #pip3 install mkdocstrings
 pip3 install mkdocstrings-python-legacy
 pip3 install mike
