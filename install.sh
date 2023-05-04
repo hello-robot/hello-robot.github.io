0f97c089b1761a5270ec89b783789acbf27d94b3
Binit Shah <bshah@hello-robot.com>
2023-05-04 09:53:14 -0700

    Update install.sh to include upgrade flag

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-pip3 install mkdocs
-pip3 install mkdocs-material
-pip3 install mkdocs-simple-plugin
-#pip3 install mkdocstrings==0.17.0
-pip3 install pytkdocs[numpy-style]
-pip3 install jinja2==3.0.3
-pip3 install Pygments
-#pip3 install mkdocstrings
-pip3 install mkdocstrings-python-legacy
-pip3 install mike
-pip3 install mkdocs-monorepo-plugin
-pip3 install mkdocs-same-dir
-pip3 install mkdocs-redirects
+pip3 install -U mkdocs
+pip3 install -U mkdocs-material
+pip3 install -U mkdocs-simple-plugin
+#pip3 install -U mkdocstrings==0.17.0
+pip3 install -U pytkdocs[numpy-style]
+pip3 install -U jinja2==3.0.3
+pip3 install -U Pygments
+#pip3 install -U mkdocstrings
+pip3 install -U mkdocstrings-python-legacy
+pip3 install -U mike
+pip3 install -U mkdocs-monorepo-plugin
+pip3 install -U mkdocs-same-dir
+pip3 install -U mkdocs-redirects
